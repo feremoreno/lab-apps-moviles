db9246700017bc572dbee23d971685acc777e2a8
Fernando M <morenofernando@yahoo.com>
2024-03-03 19:40:22 -0600

    Creacion de pagina Alumnos
    Se crea la paguina de Alumnos

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   },
+
 ];
 
 @NgModule({
