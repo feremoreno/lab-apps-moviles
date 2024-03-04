2ce41305875a614ef7210c171c9f640e60a45352
Fernando M <morenofernando@yahoo.com>
2024-03-03 21:43:29 -0600

    creacion del CRUD
    se adicionan, un componente, un servicio, una nueva pagina, y una interface

--- ionic-basic/src/app/alumnos/alumnos.page.ts
+++ ionic-basic/src/app/alumnos/alumnos.page.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumno } from '../interface/alumno';
+import { AlumnoService } from '../service/alumno.service';
 
 @Component({
   selector: 'app-alumnos',
-  templateUrl: './alumnos.page.html',
-  styleUrls: ['./alumnos.page.scss'],
+  templateUrl: './alumnos.page.html'
 })
+
 export class AlumnosPage implements OnInit {
 
-  constructor() { }
+  alumnos:Alumno[]= [];
+  nombre: string = '';
+  matricula: string ='';
+  estado: string = '';
+  idActualizar: any;
+  error: boolean = false;
+
+
+  constructor(private alumnoService: AlumnoService) { }
 
   ngOnInit() {
+    this.alumnoService.setAlumnos([
+      {id:1, nombre: 'Luis Fernando'
+      ,matricula: '1967188'},
+      {id:2, nombre: 'Paola Alejandra',
+      matricula: '2044660'},
+      {id:3, nombre: 'Juan',
+      matricula: '1967189'}
+    ]);
+
+    this.alumnos = this.alumnoService.getAlumos();
+    this.estado ='guardar';
+  }
+
+  public guardar(){
+    if( (this.nombre == undefined || this.nombre == '' ) || 
+      (this.matricula == undefined || this.matricula == '') ) {
+      this.error = true;
+      return;
+    }  
+    let alumno: Alumno={
+      nombre: this.nombre,
+      matricula: this.matricula
+    };
+
+    if (this.estado ==='actualizar'){
+      alumno.id = this.idActualizar;
+      this.alumnos = this.alumnoService.actualiza(alumno);
+    }
+    if(this.estado === 'guardar'){
+      this.alumnoService.agregarAlumno(alumno);
+      this.alumnos = this.alumnoService.getAlumos();
+    }
+    this.cancelar();
+  }
+
+  public cancelar(){
+    this.estado = 'guardar';
+    this.matricula = '';
+    this.nombre = '';
+    this.error = false;
+  }
+
+  public eliminar(id:number){
+    this.alumnoService.borrarAlumno(id);
+    this.alumnos = this.alumnoService.getAlumos();
+  }
+
+  public editar(alumno:Alumno){
+    this.estado = 'actualizar';
+    this.matricula = alumno.matricula;
+    this.nombre = alumno.nombre;
+    this.idActualizar = alumno.id;
   }
 
 }
